198cfc618e4d11550430f802608187e365a6a4ee
muslim2210 <bukhori.muslim2210@gmail.com>
2024-03-18 08:48:33 +0700

    auth register

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,29 +1,47 @@
+"use client";
+import { push } from "firebase/database";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function RegisterPage() {
+  const { push } = useRouter();
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    setError("");
+    setIsLoading(true);
+    const res = await fetch("/api/auth/register", {
+      method: "POST",
+
+      body: JSON.stringify({
+        fullname: e.target.fullname.value,
+        email: e.target.email.value,
+        password: e.target.password.value,
+      }),
+    });
+    if (res.status === 200) {
+      e.target.reset();
+      setIsLoading(false);
+      push("/login");
+    } else {
+      setError("Email already exists");
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <div className="w-full h-screen bg-gray-200 flex justify-center items-center">
-      <div className="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm p-4 sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700 w-full">
-        <form className="space-y-6" action="#">
+    <div className="w-full h-screen bg-gray-200 flex justify-center items-center flex-col">
+      {error !== "" && (
+        <div className="text-xl font-bold text-red-600">{error}</div>
+      )}
+      <div className="bg-white shadow-md border border-gray-200 rounded-lg mt-2 max-w-sm p-4 sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700 w-full">
+        <form className="space-y-6" onSubmit={(e) => handleSubmit(e)}>
           <h3 className="text-2xl font-medium text-center text-gray-900 dark:text-white">
             Register to our platform
           </h3>
-          <div>
-            <label
-              htmlFor="email"
-              className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300"
-            >
-              Your email
-            </label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-              placeholder="name@company.com"
-              required
-            />
-          </div>
+
           <div>
             <label
               htmlFor="text"
@@ -37,6 +55,22 @@ export default function RegisterPage() {
               id="fullname"
               className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
               placeholder="your fullname here"
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="email"
+              className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300"
+            >
+              Your email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              placeholder="name@company.com"
               required
             />
           </div>
@@ -58,10 +92,11 @@ export default function RegisterPage() {
           </div>
 
           <button
+            disabled={isLoading}
             type="submit"
             className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
-            Sign up account
+            {isLoading ? "Loading..." : "Sign up account"}
           </button>
           <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
             All Ready Have Account?{" "}
